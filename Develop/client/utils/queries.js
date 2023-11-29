87f6d48d18d866b5aff717cf365e62a7ef585813
Shaun Swinehart <shaun.leroyhigh@hotmail.com>
2023-11-28 22:21:27 -0600

    added auth, mutations and queries into new utils folder

---
+++ Develop/client/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+// Query to fetch the current user
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
